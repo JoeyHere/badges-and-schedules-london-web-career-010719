7886ab2b0f9bf2ba8f385dfc60a58d8e610403d8
Joseph O'Dell <josephodell@outlook.com>
2018-12-06 17:13:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+newarray = []
+  array.each do |x|
+    newarray << badge_maker(x)
+  end
+  newarray
+end
+
+def assign_rooms(array)
+  rooms=(1..7).to_a
+  array.each do |x|
+    
